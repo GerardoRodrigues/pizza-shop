11676c75d261fe58b40cbf915b969cf7d0a1011b
Gerardo Rodrigues <gerardofilho56@gmail.com>
2025-02-27 10:08:02 -0300

    conclusao cabecalho

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
 
 import { router } from "./routes";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
       <Toaster richColors closeButton />
       <RouterProvider router={router}></RouterProvider>
-    </>
+    </ThemeProvider>
   );
 }
