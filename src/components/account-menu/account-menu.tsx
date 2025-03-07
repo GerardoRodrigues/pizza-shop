c09bcb2418df3f12ebdcbc11a7f2089c87bf013b
Gerardo Rodrigues <gerardofilho56@gmail.com>
2025-03-07 12:04:09 -0300

    perfil no cabeçalho

--- src/components/account-menu/account-menu.tsx
+++ src/components/account-menu/account-menu.tsx
@@ -1,34 +1,61 @@
+import { useQuery } from "@tanstack/react-query";
 import { Building, ChevronDown, LogOut } from "lucide-react";
+
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
+import { getProfile } from "@/api/get-profile";
+
 import { Button } from "../ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 export function AccountMenu() {
-    return(
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" className="flex items-center gap-2 select-none">
-                    Pizza Shop
-                    <ChevronDown className="w-4 h-4"/>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-56">
-                <DropdownMenuLabel className="flex flex-col">
-                    <span>Gerardo Rodrigues</span>
-                    <span className="text-xs font-normal text-muted-foreground">gerardofilho56@gmail.com</span>
-                </DropdownMenuLabel>
+  const { data: profile } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile
+  })
 
-                <DropdownMenuSeparator />
+  const { data: restaurant } = useQuery({
+    queryKey: ["restaurant"],
+    queryFn: getManagedRestaurant
+  })
 
-                <DropdownMenuItem>
-                    <Building className="w-4 h-4 mr-2"/>
-                    <span>Perfil da loja</span>
-                </DropdownMenuItem>
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex items-center gap-2 select-none"
+        >
+          {restaurant?.name}
+          <ChevronDown className="w-4 h-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>{profile?.name}</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            {profile?.email}
+          </span>
+        </DropdownMenuLabel>
 
-                <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-                    <LogOut className="w-4 h-4 mr-2"/>
-                    <span>Sair</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem>
+          <Building className="w-4 h-4 mr-2" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="w-4 h-4 mr-2" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
